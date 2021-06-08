a52f67b2feaa01e015f6ed83b9785aa2e4abf3c3
Al Amin Kabbo <83044858+AlAminKabbo@users.noreply.github.com>
2021-06-09 00:14:50 +0600

    arithmeticOperator

---
+++ arithmeticOperator.js
@@ -0,0 +1,21 @@
+let a=7
+let b=9
+let c=3
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+
+console.log(a%b)
+console.log(a**b)
+
+console.log(a)
+console.log(a++)
+console.log(a)
+
+console.log(b)
+console.log(++b)
+console.log(b)
+
+console.log(--c)
+console.log(c--);
