b2b9dacbcf96d023967e4d641d6a748b50c82309
Al Amin Kabbo <83044858+AlAminKabbo@users.noreply.github.com>
2021-06-09 01:23:20 +0600

    ternaryOperator

---
+++ ternaryOperator.js
@@ -0,0 +1,6 @@
+// if a customer has more then 100 points,
+// they are a 'gold' customer, other wise,
+// they are a 'silver customer
+let points=12;
+let type= points>100 ? 'Gold Customer' : 'Silver Customer';
+console.log(type);
