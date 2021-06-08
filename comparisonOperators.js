020074d0da09141d6db6d513549a5819a9823a93
Al Amin Kabbo <83044858+AlAminKabbo@users.noreply.github.com>
2021-06-09 00:45:53 +0600

    comparisonOperators

---
+++ comparisonOperators.js
@@ -0,0 +1,13 @@
+let a=1
+console.log(a>1)
+console.log(a>=1);
+console.log(a<1);
+console.log(a<=1)
+console.log(a===1);// valu+type must be equal 
+console.log(a!==1);
+console.log(a==1);// value or type must be 
+console.log(a!=1);
+
+console.log(a==='1');// valu+type must be equal
+console.log(a=='1');// value or type must be 
+
