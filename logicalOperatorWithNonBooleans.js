905f3af820368a7fab77f6ade3076b264dafe331
Al Amin Kabbo <83044858+AlAminKabbo@users.noreply.github.com>
2021-06-09 23:35:49 +0600

    Logical Operator With Non Booleans

---
+++ logicalOperatorWithNonBooleans.js
@@ -0,0 +1,11 @@
+let highIncome= true;
+let goofCreditScore = false;
+let eligibleForLoan = highIncome && goofCreditScore; //Logical AND
+console.log(eligibleForLoan);
+
+let EligibleForLoan = highIncome || goofCreditScore; //Logical OR
+console.log(EligibleForLoan);
+
+let a=1
+let b=2
+console.log(a!=b);
