bc5d1222f7495ccf72831a7b436dc205187e571e
Al Amin Kabbo <83044858+AlAminKabbo@users.noreply.github.com>
2021-06-10 21:15:16 +0600

    if Else

---
+++ ifElse.js
@@ -0,0 +1,10 @@
+let hour=20
+if (hour<=6 && hour<=12){
+    console.log('Good Morning');
+}
+else if(hour>=13 && hour <= 18){
+    console.log('Good Afternoon');
+}
+else{
+    console.log('Good Evening');
+}
