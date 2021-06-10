0762905ff07fc6e5de75c39e93999bca94955fe1
Al Amin Kabbo <83044858+AlAminKabbo@users.noreply.github.com>
2021-06-10 21:27:00 +0600

    switch Case

---
+++ switchCase.js
@@ -0,0 +1,14 @@
+let roll=5
+switch(roll){
+    case 1:
+        console.log('Joy');
+        break;
+    case 5:
+        console.log('Nitish');
+        break;
+    case 23:
+        console.log('Kabbo');
+        break;
+    default:
+        console.log('Unknown User');
+}
