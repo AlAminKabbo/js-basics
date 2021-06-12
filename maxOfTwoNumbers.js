3b5c2757949755564708b18cbb3a952c49e1b242
Al Amin Kabbo <83044858+AlAminKabbo@users.noreply.github.com>
2021-06-13 03:01:49 +0600

    Max of two number

---
+++ maxOfTwoNumbers.js
@@ -0,0 +1,16 @@
+let num1 = prompt('Enter first number: ');
+let num2 = prompt('Enter second number: ');
+let number= max(num1,num2)
+console.log('Max= '+number);
+
+function max(a,b){
+    if (a>b) return a;
+    else return b;
+}
+
+let n= Max(9,7)
+    console.log(n);
+
+function Max(x,y){
+    return (x>y ? x:y)
+}
