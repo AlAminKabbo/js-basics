2ddee5e1145ec6cb1098d3a6850d6031fab9cb6d
Al Amin Kabbo <83044858+AlAminKabbo@users.noreply.github.com>
2021-06-13 01:03:08 +0600

    for in Loops

---
+++ forInLoops.js
@@ -0,0 +1,12 @@
+const person={
+    Name: 'Kabbo',
+    Roll: 23,
+}
+for(let key in person){
+    console.log(key,person[key]);
+}
+
+let colors=['red','blue','green']
+for(let index in colors){
+    console.log(index,colors[index])
+}
