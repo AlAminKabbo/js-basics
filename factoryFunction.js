460a2fc9510e9c2ee633cc8274cec78106819a38
Al Amin Kabbo <83044858+AlAminKabbo@users.noreply.github.com>
2021-06-18 01:23:10 +0600

    Factory Function

---
+++ factoryFunction.js
@@ -0,0 +1,15 @@
+// Factory Function
+function createCircle(radius) {
+    return{
+        radius,
+        draw(){
+            console.log('draw');
+        }
+    };
+}
+
+const circle1= createCircle(1);
+console.log(circle1);
+
+const circle2= createCircle(2);
+console.log(circle2);
