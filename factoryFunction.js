c589b31ca2b7c30b6f446d32458a6e766087365c
Al Amin Kabbo <83044858+AlAminKabbo@users.noreply.github.com>
2021-09-19 02:50:17 +0600

    ud

--- factoryFunction.js
+++ factoryFunction.js
@@ -12,4 +12,4 @@ const circle1= createCircle(1);
 console.log(circle1);
 
 const circle2= createCircle(2);
-console.log(circle2);
+console.log(circle2 );
---
+++ priceRange.js
@@ -0,0 +1,17 @@
+let price = {
+    oneDigit:{
+        a: 2,
+        b: 3,
+        c: 9,
+    },
+    twoDigit: {
+        d: 10,
+        e: 53,
+        f: 89,
+    },
+    threeDigit:{
+        g: 102,
+        h: 209,
+        i: 909,
+    },
+}
