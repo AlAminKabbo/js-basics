37e26cc079d181b3d5d903bd117ac01ce98cefde
Al Amin Kabbo <83044858+AlAminKabbo@users.noreply.github.com>
2021-09-28 01:15:13 +0600

    Create Mutate an Array Declared with const.js

---
+++ Mutate an Array Declared with const.js
@@ -0,0 +1,9 @@
+const s = [5, 7, 2];
+function editInPlace() {
+  s[0] = 2;// Only change code below this line
+
+  s[1] = 5// Using s = [2, 5, 7] would be invalid
+
+  s[2] = 7// Only change code above this line
+}
+editInPlace();
