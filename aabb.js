a9ba6b0dddb7f0eda8f616700bda8232875e98f9
Al Amin Kabbo <83044858+AlAminKabbo@users.noreply.github.com>
2021-09-29 01:22:35 +0600

    aabb

---
+++ aabb.js
@@ -0,0 +1,10 @@
+var data = window.prompt("Enter your Data: ");
+name = data.toLowerCase();
+
+if(name == 'aab'){
+    console.log("Accepted...");
+}else if(name == 'aabb'){
+    console.log("Accepted...");
+}else{
+    console.log("Not Accepted!!");
+}
