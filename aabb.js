8a05e741d159afe7321c58c44beaeee222daee2d
Al Amin Kabbo <83044858+AlAminKabbo@users.noreply.github.com>
2021-09-29 03:36:53 +0600

    aabb

--- aabb.js
+++ aabb.js
@@ -1,10 +1,23 @@
-var data = window.prompt("Enter your Data: ");
+let data = window.prompt("Enter your Data: ");
 name = data.toLowerCase();
 
-if(name == 'aab'){
+const string = name;
+
+const usingSplit = string.split('');
+const usingSpread = [...string];
+const usingArrayFrom = Array.from(string);
+if(usingArrayFrom[0] == 'a' && usingArrayFrom[1] == 'a' && usingArrayFrom[2] == 'b' && usingArrayFrom.length <= 3){
     console.log("Accepted...");
-}else if(name == 'aabb'){
-    console.log("Accepted...");
+}else if (usingArrayFrom.length > 3){
+    for(i = 3; i < usingArrayFrom.length;i++){
+        var j = usingArrayFrom.length - 1;
+        if(usingArrayFrom[j] != 'b'){
+            console.log("Not Accepted!!");
+            break;
+        }else(usingArrayFrom[i] == 'b')
+            console.log("Accepted...");
+            break;
+    }
 }else{
     console.log("Not Accepted!!");
 }
---
+++ test.js
@@ -0,0 +1,7 @@
+const string = 'word';
+
+const usingSplit = string.split('');
+const usingSpread = [...string];
+const usingArrayFrom = Array.from(string);
+
+console.log({usingArrayFrom});
